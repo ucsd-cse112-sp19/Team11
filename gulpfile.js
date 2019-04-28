34f013e2e94f55b111e9813cc9a67815e0970a3c
Richard Pena <penarichard53@gmail.com>
2019-04-28 16:54:32 -0700

    gulp clean will remove the minfied JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp')
+var del = require('del')
 var uglify = require('gulp-uglify')
+
+
+gulp.task('clean', () => del(['dist']))
+
 
 gulp.task('uglify', function() {
   return gulp.src('./src/*.js')
