41c6f9a7a40f047aaf6f679494332baa4f5df02f
Richard Pena <penarichard53@gmail.com>
2019-04-27 18:09:58 -0700

    example js file to test documentation generator

---
+++ src/example.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/**
+ * This functions adds one to its input
+ * @param {number} input any number
+ * @returns {number} that number, plus one
+ */
+function addOne (input) {
+  return input + 1
+}
+
+/**
+ * This function adds two to its input
+ * @param {number} input any number
+ * @returns {number} that number, plus two
+ */
+function addTwo (x) {
+  return x + 2
+}
