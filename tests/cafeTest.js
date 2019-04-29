c6a253d1b7411221b46aaac21acc0de5bbde5b86
Ricardo Molina <R3molina@.ucsd.edu>
2019-04-28 19:11:00 -0700

    testing

--- tests/cafeTest.js
+++ tests/cafeTest.js
@@ -1,9 +1,16 @@
-import { Selector } from 'testcafe';
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`;
 
-test("My first wee baby test", async t =>{
-    await t
-        .typeText('#developer-name', 'John Smith')
-        .click('#submit-button');
-});
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { Selector } from 'testcafe'
+fixture`Getting Started`
+  .page`http://devexpress.github.io/testcafe/example`
+
+test('My first wee baby test', async t => {
+  await t
+    .typeText('#developer-name', 'John Smith')
+    .typeText('#comments', 'helllo I hope dis works or else im totally lost')
+    .click('#submit-button')
+    .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!')
+  // const articleHeader = await Selector('.result-content').find('h1')
+  // let headerText = await articleHeader.innerText
+})
