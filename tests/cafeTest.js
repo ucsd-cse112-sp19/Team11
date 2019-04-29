cf566887db9c809548586a99dc6250ebcabd4561
Ricardo Molina <R3molina@.ucsd.edu>
2019-04-29 15:57:51 -0700

    need a webpage

--- tests/cafeTest.js
+++ tests/cafeTest.js
@@ -3,14 +3,15 @@
 /* eslint-disable no-unused-vars */
 import { Selector } from 'testcafe'
 fixture`Getting Started`
-  .page`http://devexpress.github.io/testcafe/example`
+  .page`hfile:///Users/ricardo/Documents/Projects/WebComponents/Team11/src/hello.html`
 
-test('My first wee baby test', async t => {
+test('test', async t => {
   await t
-    .typeText('#developer-name', 'John Smith')
-    .typeText('#comments', 'helllo I hope dis works or else im totally lost')
-    .click('#submit-button')
-    .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!')
+    .expect(Selector('#hello').innerText).eql('blackpink')
+    //  .typeText('#developer-name', 'John Smith')
+    //  .typeText('#comments', 'helllo I hope dis works or else im totally lost')
+    //  .click('#submit-button')
+    //  .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!')
   // const articleHeader = await Selector('.result-content').find('h1')
   // let headerText = await articleHeader.innerText
 })
