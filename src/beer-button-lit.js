e2f0e67fd1d429a641b07e1e0f8f4824b4b67cb7
Gordon Hu <gohu@ucsd.edu>
2019-05-13 18:37:45 -0700

    I'm having local git problems

--- src/beer-button-lit.js
+++ src/beer-button-lit.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit-element@2.0.1/lit-element.js?module'
 
 class BeerButtonLit extends LitElement {
+
   static get properties () {
     return {
       type: {
@@ -28,14 +29,6 @@ class BeerButtonLit extends LitElement {
       }
     }
   }
-  constructor () {
-    super()
-    this.type = '' // Default
-    this.text = 'Click Me'
-    this.size = ''
-    this.disabled = false
-    this.loading = false
-  }
 
   static get styles () {
     return css`
@@ -48,11 +41,44 @@ class BeerButtonLit extends LitElement {
         border: none;
         background-color: salmon;
       }
-    `
+    `;
+  }
+
+  constructor () {
+    super()
+    this.type = '' // Default
+    this.text = 'Click Me'
+    this.size = ''
+    this.disabled = false
+
+    //checks if loading attribute exists
+    var beer_button_lit = document.getElementsByTagName('beer-button-lit').item(0);
+    var loading_attr = beer_button_lit.getAttribute("loading");
+
+    //loading attribute is present if var loading_attr is not null
+    if(loading_attr == ""){
+      this.loading = true;
+      this.disabled = true;
+      this.text = "Loading";
+    }
+
+    this.loading = false;
   }
 
   render () {
-    return html`
+
+    //if the loading attribute is set to true, render custom html
+    if(this.loading){
+      return html
+      `
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <button class="buttonload">
+      <i class="fa fa-spinner fa-spin"></i>Loading
+      </button>
+      `
+    }
+    return html
+    `
       <button>${this.text}</button>
     `
   }
