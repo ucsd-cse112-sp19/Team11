c020135721fe3de12e10e57174ecc5baec7da57e
zhaoesther <ewzhao@ucsd.edu>
2019-05-13 23:22:35 -0700

    Updated linter and resolved import issue

--- src/beer-button-lit.js
+++ src/beer-button-lit.js
@@ -1,44 +1,44 @@
-import { LitElement, html, css } from 'https://unpkg.com/lit-element@2.0.1/lit-element.js?module'
+import {LitElement, html, css} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
 class BeerButtonLit extends LitElement {
-  static get properties () {
-    return {
-      type: {
-        type: String,
-        reflect: true
-      },
-      text: {
-        type: String,
-        reflect: true // true if we are reflecting the property to an attribute
-        // Example: We can even specify how we want attributes to be reflected
-        // attribute: todo
-        // <to-do-item todo="Finish blog"></to-do-item>
-      },
-      size: {
-        type: String, // Accepted values: medium, small, mini
-        reflect: true
-      },
-      disabled: {
-        type: Boolean,
-        reflect: true
-      },
-      loading: {
-        type: Boolean,
-        reflect: true
-      }
+    static get properties() {
+        return {
+            type: {
+                type: String,
+                reflect: true
+            },
+            text: {
+                type: String,
+                reflect: true // true if we are reflecting the property to an attribute
+                // Example: We can even specify how we want attributes to be reflected
+                // attribute: todo
+                // <to-do-item todo="Finish blog"></to-do-item>
+            },
+            size: {
+                type: String, // Accepted values: medium, small, mini
+                reflect: true
+            },
+            disabled: {
+                type: Boolean,
+                reflect: true
+            },
+            loading: {
+                type: Boolean,
+                reflect: true
+            }
+        };
     }
-  }
-  constructor () {
-    super()
-    this.type = '' // Default
-    this.text = 'Click Me'
-    this.size = ''
-    this.disabled = false
-    this.loading = false
-  }
+    constructor() {
+        super();
+        this.type = ""; // Default
+        this.text = "Click Me";
+        this.size = "";
+        this.disabled = false;
+        this.loading = false;
+    }
 
-  static get styles () {
-    return css`
+    static get styles() {
+        return css`
       :host {
         display: block;
         font-family: sans-serif;
@@ -48,13 +48,13 @@ class BeerButtonLit extends LitElement {
         border: none;
         background-color: salmon;
       }
-    `
-  }
+    `;
+    }
 
-  render () {
-    return html`
+    render() {
+        return html`
       <button>${this.text}</button>
-    `
-  }
+    `;
+    }
 }
-customElements.define('beer-button-lit', BeerButtonLit)
+customElements.define("beer-button-lit", BeerButtonLit);
