296e21a7a8f1aa9abd478fefa7bca31e6c6f7eee
Ricardo Molina <R3molina@.ucsd.edu>
2019-05-14 10:12:35 -0700

    custom functions working

--- src/Raw-Button/raw-button.js
+++ src/Raw-Button/raw-button.js
@@ -17,15 +17,29 @@ template.innerHTML = `
 class BeerButton extends HTMLElement {
     constructor() {
         super();
+        const main = document.querySelector("main");
+        
         this._shadowRoot = this.attachShadow({"mode" : "open"});
         
-        // this is the piece of code that takes all that html stuff up top and makes it visible
+        // this is the piece of code that takes all that html stuff up top and makes 
+        // it visible
         this._shadowRoot.appendChild(template.content.cloneNode(true));
         this.$beerButton = this._shadowRoot.querySelector("button");
+        this.$rawButton = document.querySelector("beer-button");
         
-        // else if(attribute == maybe just maybe we can take in a function from the user hmmmmmmm, Ill think more on this){
+        // TODO when we get the ID functionality working this should get moved into a 
+        // function 
+        if(this.$rawButton.innerHTML.length > 0){
+            console.log(this.$rawButton.innerHTML);
+            console.log(this.$beerButton.innerHTML);
+            this.$beerButton.innerHTML = this.$rawButton.innerHTML;
+        }
+        
+        // else if(attribute == maybe just maybe we can take in a function from 
+        //  the user hmmmmmmm, Ill think more on this){
         //  execute code
         // }
+        // TODO DELETE THE ABOVE COMMENTS BEFORE MERGING TO DEV!!!
         if(!this.hasAttribute("disable")){
             if(this.hasAttribute("link")){
                 this.$beerButton.addEventListener("click", this.linkFunction.bind(this));
@@ -42,11 +56,41 @@ class BeerButton extends HTMLElement {
                 this.inc = false;
                 this.$beerButton.addEventListener("click", this.incrementDecrementFunction.bind(this));
             }
+            else if(this.hasAttribute("script") && this.hasAttribute("functionName")){
+                this.$beerButton.addEventListener("click", this.customFunction.bind(this));
+            }
         }
     } 
+
+    /**
+     * @description: Takes a script and function then loads the function from 
+     * the script so that it can be used with the button
+     * @return void
+     */
+    customFunction(){
+        var scriptName = this.getAttribute("script")
+        var functionName = this.getAttribute("functionName")  
+        if(scriptName.length > 0){
+            import(scriptName).then(script =>{
+                if(functionName.length > 0 ){
+                    //script.loadPageInto(this.main);
+                    script[functionName]();
+                }
+                else{
+                    throw "FunctionName cant be null";
+                }
+            });
+        }
+        else{
+        throw "Script cant be null"; 
+         }
+           
+         
+    }
         
     /**
-     * @description : Takes a link and when the button is clicked will navigate the browser to the new link
+     * @description : Takes a link and when the button is clicked will navigate 
+     * the browser to the new link
      * @return : void
      */
     linkFunction(){
@@ -75,8 +119,9 @@ class BeerButton extends HTMLElement {
                 console.log( "Invalid input");
             }
             else{
-            // take the field and parse it into an integer and then perform the increment operation
-            // on that returned value and then set this as the new value 
+            // take the field and parse it into an integer and then perform the 
+            // increment operation on that returned value and then set this as 
+            // the new value 
                 value = parseInt(document.getElementById(incrementId).value, 10);
                 value ++;
                 document.getElementById(incrementId).value = value;
@@ -89,8 +134,9 @@ class BeerButton extends HTMLElement {
             if( decrementId <= 0 ){
                 console.log( "Invalid input");
             }
-            // take the field and parse it into an integer and then perform the decrement operation
-            // on that returned value and then set this as the new value 
+            // take the field and parse it into an integer and then perform 
+            // the decrement operationon that returned value and then set this 
+            // as the new value 
             value = parseInt(document.getElementById(decrementId).value, 10);
             value --;
             document.getElementById(decrementId).value = value;
