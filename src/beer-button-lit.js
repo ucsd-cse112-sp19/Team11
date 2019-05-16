248dac24c8b7d235633eca58098bc7bd60d75654
amossennis <amossenn@ucsd.edu>
2019-05-16 15:44:50 -0700

    testing stuff

--- src/beer-button-lit.js
+++ src/beer-button-lit.js
@@ -2,66 +2,64 @@ import {LitElement, html, css} from "https://unpkg.com/lit-element@2.0.1/lit-ele
 
 // Color of button depending on type attribute
 const types = [
-    {type: "primary", bgColor: "#409EFF"}, 
-    {type: "success", bgColor: "#67C23A"}, 
-    {type: "info",    bgColor: "#909399"}, 
-    {type: "warning", bgColor: "#F4A338"}, 
-    {type: "danger",  bgColor: "#F56C6C"}
+    {type: "primary", bgColor: "#409EFF", bgLighter:"#8CC5FF", bgPlain: "#ECF5FF"}, 
+    {type: "success", bgColor: "#67C23A", bgLighter:"#A3DA88", bgPlain: "#F0F9EB"}, 
+    {type: "info",    bgColor: "#909399", bgLighter:"#BCBEC2", bgPlain: "#F4F4F5"}, 
+    {type: "warning", bgColor: "#F4A338", bgLighter:"#F8C887", bgPlain: "#FDF6EC"}, 
+    {type: "danger",  bgColor: "#F56C6C", bgLighter:"#F9A6A6", bgPlain: "#FEF0F0"}
 ];
 
-// filter: brightness(85%);
+// Index to keep track of which beer-button-lit component in a given HTML page
+// when there are multiple
+var idx = 0;
 
 class BeerButtonLit extends LitElement {
     static get properties() {
         return {
-            type: {
-                type: String,
-                reflect: true
-            },
-            text: {
-                type: String,
-                reflect: true // true if we are reflecting the property to an attribute
-                // Example: We can even specify how we want attributes to be reflected
-                // attribute: todo
-                // <to-do-item todo="Finish blog"></to-do-item>
-            },
-            size: {
-                type: String, // Accepted values: medium, small, mini
-                reflect: true
-            },
-            disabled: {
-                type: Boolean,
-                reflect: true
-            },
-            loading: {
-                type: Boolean,
-                reflect: true
-            },
-            mainColor: {
-                type: String
-            }
+            // reflect: true if we are reflecting the property to an attribute
+            // Example: We can even specify how we want attributes to be reflected
+            // attribute: todo
+            // <to-do-item todo="Finish blog"></to-do-item>
+            type:     {type: String, reflect: true},
+            text:     {type: String, reflect: true},
+            size:     {type: String, reflect: true},
+            disabled: {type: Boolean, reflect: true},
+            loading:  {type: Boolean, reflect: true},
+            round:    {type: Boolean, reflect: true},
+            circle:   {type: Boolean, reflect: true}
         };
     }
 
     constructor() {
         super();
-        this.type = ""; // Default
-        this.text = "Default";
+        // Default attributes
+        this.type = "";
         this.size = "";
+        this.loading = false;
         this.disabled = false;
+        this.round = false;
+        this.circle = false;
 
         // Checks if loading attribute exists
-        var beer_button_lit = document.getElementsByTagName("beer-button-lit").item(0);
+        var beer_button_lit = document.getElementsByTagName("beer-button-lit").item(idx);
+
+        // Increment the index for each new beer-button component
+        idx++;
+
+        // Set the text property with the user text in between tag
+        // <beer-button-lit>USER TEXT</beer-button-lit>
+        this.text = beer_button_lit.textContent;
         var loading_attr = beer_button_lit.getAttribute("loading");
+        var disabled_attr = beer_button_lit.getAttribute("disabled");
 
         // loading attribute is present if var loading_attr is not null
-        if(loading_attr == ""){
+        if(loading_attr == "") {
             this.loading = true;
+        }
+        if(disabled_attr == "") {
             this.disabled = true;
-            this.text = "Loading";
         }
 
-        this.loading = false;
     }
 
     static get styles() {
@@ -83,51 +81,151 @@ class BeerButtonLit extends LitElement {
       }
 
       button:hover {
-          border: 1px solid #CAE4FF;
-          background-color: #ECF5FF;
-          color: #409EFF;
-          transition: 0.05s;
-      }
-      
-      button.nonDefault:hover {
-        opacity: 0.8;
+        border: 1px solid #CAE4FF;
+        background-color: #ECF5FF;
+        color: #409EFF;
         transition: 0.05s;
-    }
+      }
+
+      button.nonDefault:hover {
+        filter: brightness(108%);
+        transition: 0.05s;
+      }
 
       button:focus {
-          outline:none;
+        outline:none;
+        filter: brightness(108%);
+      }
+
+      .round {
+        border-radius: 35px;
       }
       
+      .circle {
+        border-radius: 50%;
+        width: 3rem;
+        height: 3rem;
+      }
+
+      .medium {
+        transform: scale(0.90);
+      }
+
+      .small {
+        transform: scale(0.80);
+      }
+
+      .mini {
+        transform: scale(0.70);
+      }      
+      
+      .unclickable {
+        pointer-events:none;
+        opacity: 0.6;
+        transition: 0.05s;
+      }
     `;
+    }
+
+    /**
+     * Returns string representing CSS classes this web
+     * component will have
+     */
+    _getClass() {
+        let _class = "";
+        if(this.round) {
+            _class += "round ";
+        }
+        if(this.circle) {
+            _class += "circle ";
+        }
+        if(this.loading | this.disabled) {
+            _class += "unclickable ";
+        }
+        if(!this.isDefault) {
+            _class += "nonDefault ";
+            if (this.size == "medium") _class += "medium ";
+            else if (this.size == "small") _class += "small ";
+            else if (this.size == "mini") _class += "mini ";
+        }
+        return _class;
+    }
+
+    /**
+     * Returns string representing CSS style this web
+     * component will have
+     */
+    _getStyle() {
+        let _style = "";
+        var isDefault = this.type === ""; // Check to see if type is default
+
+        // Find the corresponding type to select appropriate button color
+        if(!isDefault) {
+            let style_background = "background-color:";
+            let style_border     = "border:";
+            let style_textColor  = "color:";
+
+            let typesItem = types.find((elem) => {
+                let match = elem.type === this.type;
+                return match;
+            });
+
+            // Generate appropriate style string
+            style_background += typesItem.bgColor + ";";
+            style_border     += "none;";
+            style_textColor  += "white;";
+
+
+            _style = style_background + style_border + style_textColor;
+        }
+        if(this.loading) {
+            _style += "cursor: default;";
+        }
+        return _style;
+    }
+
+    /**
+     * Checks of the 'type' attribute is valid.
+     * Valid types are: primary, succuess, info, warning, danger, or "" (default)
+     */
+    _validType() {
+        let valid = false;
+        types.find((elem) => {
+            if(elem.type === this.type) valid = true;
+        });
+        return valid | this.isDefault;
+    }
+
+    _clickHandler() {
+        alert("You clicked the button!");
     }
 
 
     render() {
-        var isDefault = this.type === "";
-        var typesItem = types.find((elem) => {
-            return elem.type === this.type;
-        });
-
-        if(!isDefault) {
-            var style_background = "background-color:" + typesItem.bgColor + ";";
-            var style_border     = "border:none;";
-            var style_textColor  = "color:white;";
+        if(!this._validType()) {
+            this.type = "";
         }
 
         // If the loading attribute is set to true, render custom html
-        if(this.loading){
+        if(this.loading) {
+            if(!this.circle) {
+                this.text = "Loading";
+            }
             return html`
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <button class="buttonload" class=${!isDefault ? "nonDefault" : ""} style=${!isDefault ? style_background + style_border + style_textColor: ""}>
-            <i class="fa fa-spinner fa-spin"></i>Loading
+            <button class=${this._getClass()} style=${this._getStyle()}>
+            <i class="fa fa-spinner fa-spin"></i>
+            ${this.text}
             </button>
             `;
         }
 
         return html`
-        <button class=${!isDefault ? "nonDefault" : ""} style=${!isDefault ? style_background + style_border + style_textColor: ""}>
-        ${this.text}
-        </button>
+        <div style=${this.disabled ? "cursor: not-allowed" : "" }>
+            <button class=${this._getClass()} style=${this._getStyle()} @click=${this._clickHandler}>
+            ${this.text}
+            </button>
+        </div>
         `;
     }
 }
---
+++ src/test.js
@@ -0,0 +1,124 @@
+import {Selector, ClientFunction} from "testcafe";
+
+fixture `fixture`
+   .page('http://localhost/testcafe/');
+
+
+
+/* ================================================= */
+/**
+ * MAKE SURE TO CHANGE THIS VALUE BEFORE PUSHING
+ */
+const LOCAL_DEV = true;
+/* ================================================= */
+
+
+if(!LOCAL_DEV) {
+    fixture `Getting Started`
+        .page `https://ucsd-cse112.github.io/Team11/beer-button-lit.html`;
+} else {
+    fixture `Getting Started`
+        .page `http://localhost:8080/beer-button-lit.html`;
+}
+ 
+const getPageURL = ClientFunction( () => window.location.href );
+
+
+
+const animatedButton = Selector('#animated');
+const defaultButton = Selector("#plain");
+const primaryButton = Selector("#primary");
+const successButton = Selector("#success");
+const infoButton = Selector("#info");
+const warnButton = Selector("#warning");
+const dangerButton = Selector("#danger");
+const miniButton = Selector("#mini-me");
+const smallButton = Selector("#smol");
+const mediumButton = Selector("#medi");
+const roundButton = Selector("#rounded");
+const circleButton = Selector("#circular");
+const disabledButton = Selector("#round-success");
+const breakButton = Selector("#circle-danger");
+const invalidButton = Selector("#broken");
+
+test("Attribute testing: components have correct type", async t => {
+    await t 
+        .expect(animatedButton.getAttribute("type")).eql('')
+        .expect(defaultButton.getAttribute("type")).eql('')
+        .expect(primaryButton.getAttribute("type")).eql('primary')
+        .expect(smallButton.getAttribute("type")).eql('')
+        .expect(roundButton.getAttribute("type")).eql('')
+        .expect(circleButton.getAttribute("type")).eql('');
+});
+
+test("Attribute testing: components have intended text", async t => {
+    await t
+        .expect(defaultButton.getAttribute('text')).eql('')
+        .expect(primaryButton.getAttribute('text')).eql('Primary')
+        .expect(successButton.getAttribute("text")).eql('Success')
+        .expect(mediumButton.getAttribute("text")).eql('Im Medium')
+        .expect(roundButton.getAttribute("text")).eql('beer')
+        .expect(circleButton.getAttribute("text")).eql('');
+});
+
+test("Attribute Testing: verify button size", async t => {
+    await Selector('#mini-me'); const mi = await Selector(() => document.querySelector('#mini-me').shadowRoot.querySelector('button'))
+    await Selector('#round-success'); const inv = await Selector(() => document.querySelector('#round-success').shadowRoot.querySelector('button'))
+    await Selector('#smol'); const sm = await Selector(() => document.querySelector('#smol').shadowRoot.querySelector('button'))
+    await Selector('#medi'); const me = await Selector(() => document.querySelector('#medi').shadowRoot.querySelector('button'))
+
+    await t 
+        .expect(miniButton.getAttribute("size")).eql('mini')
+        .expect(mi.getStyleProperty('transform')).eql('matrix(0.7, 0, 0, 0.7, 0, 0)')
+        .expect(disabledButton.getAttribute("size")).eql('mini')
+        .expect(inv.getStyleProperty('transform')).eql('matrix(0.7, 0, 0, 0.7, 0, 0)')
+        .expect(smallButton.getAttribute("size")).eql('small')
+        .expect(sm.getStyleProperty('transform')).eql('matrix(0.8, 0, 0, 0.8, 0, 0)')
+        .expect(mediumButton.getAttribute("size")).eql('med')
+        .expect(me.getStyleProperty('transform')).eql('none');
+}); 
+
+test("Attribute testing: verify button color", async t => {
+    await Selector('#plain'); const plain = await Selector(() => document.querySelector('#plain').shadowRoot.querySelector('button'))
+    await Selector('#primary'); const primary = await Selector(() => document.querySelector('#primary').shadowRoot.querySelector('button'))
+    await Selector('#success'); const success = await Selector(() => document.querySelector('#success').shadowRoot.querySelector('button'))
+    await Selector('#info'); const info = await Selector(() => document.querySelector('#info').shadowRoot.querySelector('button'))
+    await Selector('#warning'); const warning = await Selector(() => document.querySelector('#warning').shadowRoot.querySelector('button'))
+    await Selector('#danger'); const danger = await Selector(() => document.querySelector('#danger').shadowRoot.querySelector('button'))
+
+    await t 
+        .expect(plain.getStyleProperty("background-color")).eql('rgb(255, 255, 255)')
+        .expect(primary.getStyleProperty("background-color")).eql('rgb(64, 158, 255)')
+        .expect(success.getStyleProperty("background-color")).eql('rgb(103, 194, 58)')
+        .expect(info.getStyleProperty("background-color")).eql('rgb(144, 147, 153)')
+        .expect(warning.getStyleProperty("background-color")).eql('rgb(244, 163, 56)')
+        .expect(danger.getStyleProperty("background-color")).eql('rgb(245, 108, 108)');
+});
+
+test('Function testing: loading buttons are animated and disabled', async t => {
+    await Selector('#animated'); const text = await Selector(() => document.querySelector('#animated').shadowRoot.querySelector('button'))
+    await Selector('#animated'); const ani = await Selector(() => document.querySelector('#animated').shadowRoot.querySelector('i'))
+    await t
+        .expect(text.innerText).eql(' Loading')
+        //.expect(ani.hasClass('fa fa-spinner fa-spin')).eql(true)
+        .setNativeDialogHandler(() => false)
+        .click('#animated');
+});
+
+test('Function testing: click creates an alert', async t => {
+    await t
+        .setNativeDialogHandler(() => true)
+        .click('#plain')
+        .click('#mini-me')
+        .click('#smol')
+        .click('#medi')
+        .click('#rounded')
+        .click('#circular');
+});
+
+test('Function testing: cannot click disabled buttons', async t => {
+    await t
+        .setNativeDialogHandler(() => false)
+        .click('#round-success')
+        .click('#animated')
+});
