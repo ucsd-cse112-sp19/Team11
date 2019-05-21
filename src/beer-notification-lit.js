5fea224dc0ddd685bcf9e19b5b14909abc8af642
Donghak Oh <d4oh@ucsd.edu>
2019-05-21 14:36:13 -0700

    animation created

--- src/beer-notification-lit.js
+++ src/beer-notification-lit.js
@@ -5,7 +5,7 @@ var types = [
     {type: "warning", svg: "./svg/notif-icon-warning.svg"},
     {type: "danger",  svg: "./svg/notif-icon-danger.svg"},
     {type: "info",    svg: "./svg/notif-icon-info.svg"},
-];
+];    
 
 // Index to keep track of which beer-notication-lit component in a given HTML page
 // when there are multiple
@@ -34,9 +34,7 @@ class BeerNotificationLit extends LitElement {
 
             // Notification's offset from the edge of the screen
             // Note that every Notification instance of the same moment should have the same offset
-            offset: {type: Number, reflect: true}
-
-
+            offset: {type: Number, reflect: true},
             // TODO: Add properties as needed
         };
     }
@@ -63,12 +61,11 @@ class BeerNotificationLit extends LitElement {
 
     constructor() {
         super();
-
+        
         //our precious beer-notification
         var beer_notification_lit = document.getElementsByTagName("beer-notification-lit").item(idx);
-
-        // Increment the index for each new beer-button component in case of multiple
         idx++;
+        // Increment the index for each new beer-button component in case of multiple
 
         // Default attributes
         this.type = "";
@@ -133,7 +130,7 @@ class BeerNotificationLit extends LitElement {
             width: 20em;         /* Fixed width */
             height: auto;        /* Dynamically adjust height based on content */
             overflow: auto;      /* Enable scroll if needed */
-            margin: 2em;
+            margin-top: 2em;
             border-radius: 0.5em;
             background-color: white;
         }
@@ -213,10 +210,37 @@ class BeerNotificationLit extends LitElement {
         // _this is used instead of this because this will reference the window
         _this.parentNode.removeChild(_this);
     }
+    
+    _getStyle() {
+        let _style = "z-index: 1; right: 0; top: 0;width: 20em; height: auto; margin-top:2em; overflow: auto; border-radius: 0.5em; background-color: white;";
+        
+        let topMargin = "";
+
+        idx++;
+        
+        let margin = idx - 4;
+        
+        margin *= 9;
+        
+        topMargin += margin.toString(10);
+        _style += "margin-top:" + topMargin +"em;";
+        
+        return _style;
+    }
+    
+    _getMargin() {
+        let margin = "";
+        
+        let number = idx * 2;
+        
+        margin = number.toString(10);
+        
+        return margin;
+    }
 
     render() {
         return html`
-        <div class="popup shadow">
+        <div class="popup shadow" style=${this._getStyle()}>
             <div class="popup-content">
                 <span>
                     <img class=${this._validType() ? "icon" : ""} 
@@ -225,7 +249,7 @@ class BeerNotificationLit extends LitElement {
                 </span>
                 <p class="popup-title">${this.title}</> 
                 <span class="close">&times;</span>
-                <p>${this.message}</>
+                <p>${this.message + this._getMargin()}</>
             </div>
         </div>
     `;
