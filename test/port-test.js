72fe8e7d044debde7785ee27821a17e6663a3895
huynhy1 <eric.k.huynh@protonmail.com>
2019-05-21 08:52:00 -0700

    Round/Circle button tests

--- test/port-test.js
+++ test/port-test.js
@@ -11,7 +11,7 @@ fixture `fixture`
 /**
  * MAKE SURE TO CHANGE THIS VALUE BEFORE PUSHING
  */
-const LOCAL_DEV = false;
+const LOCAL_DEV = true;
 /* ================================================= */
 
 
@@ -95,6 +95,29 @@ test("Attribute testing: verify button color", async t => {
         .expect(info.getStyleProperty("background-color")).eql("rgb(144, 147, 153)")
         .expect(warning.getStyleProperty("background-color")).eql("rgb(244, 163, 56)")
         .expect(danger.getStyleProperty("background-color")).eql("rgb(245, 108, 108)");
+});
+
+test("Attribute testing: verify button roundness", async t => {
+    await Selector("#round-success"); const success = await Selector(() => document.querySelector("#round-success").shadowRoot.querySelector("button"));
+    await Selector("#rounded"); const round = await Selector(() => document.querySelector("#rounded").shadowRoot.querySelector("button"));
+    await Selector("#circular"); const circle = await Selector(() => document.querySelector("#circular").shadowRoot.querySelector("button"));
+
+    await t 
+        .expect(success.hasClass("round")).ok()
+        .expect(success.getStyleProperty("border-bottom-left-radius")).eql("35px")
+        .expect(success.getStyleProperty("border-bottom-right-radius")).eql("35px")
+        .expect(success.getStyleProperty("border-top-left-radius")).eql("35px")
+        .expect(success.getStyleProperty("border-bottom-right-radius")).eql("35px")
+        .expect(round.hasClass("round")).ok()
+        .expect(round.getStyleProperty("border-bottom-left-radius")).eql("35px")
+        .expect(round.getStyleProperty("border-bottom-right-radius")).eql("35px")
+        .expect(round.getStyleProperty("border-top-left-radius")).eql("35px")
+        .expect(round.getStyleProperty("border-bottom-right-radius")).eql("35px")
+        .expect(circle.hasClass("circle")).ok()
+        .expect(circle.getStyleProperty("border-bottom-left-radius")).eql("50%")
+        .expect(circle.getStyleProperty("border-bottom-right-radius")).eql("50%")
+        .expect(circle.getStyleProperty("border-top-left-radius")).eql("50%")
+        .expect(circle.getStyleProperty("border-bottom-right-radius")).eql("50%");
 });
 
 test("Function testing: loading buttons are animated and disabled", async t => {
