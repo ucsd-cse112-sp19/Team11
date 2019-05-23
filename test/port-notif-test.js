3b4a2467d6e7497c483aaa56c0d89fc75029ce1b
huynhy1 <eric.k.huynh@protonmail.com>
2019-05-23 13:42:27 -0700

    tried to merge Austin's tests with Coder branch

---
+++ test/port-notif-test.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import {Selector, ClientFunction} from "testcafe";
+
+fixture `fixture`
+    .page("http://localhost/testcafe/");
+
+
+
+/* ================================================= */
+/**
+ * MAKE SURE TO CHANGE THIS VALUE BEFORE PUSHING
+ */
+const LOCAL_DEV = true;
+/* ================================================= */
+
+
+if(!LOCAL_DEV) {
+    fixture `Getting Started`
+        .page `https://ucsd-cse112.github.io/Team11/beer-notification-lit.html`;
+} else {
+    fixture `Getting Started`
+        .page `http://127.0.0.1:8080/test-fixtures/beer-notification-lit-test.html`;
+}
+ 
+const getPageURL = ClientFunction( () => window.location.href );
+
+const indefButton = Selector("#indefinite");
+const indefSpan = Selector("#indefiniteSpan");
+const defButton = Selector("#definite");
+const defSpan = Selector("#definiteSpan");
+
+test("Click indefinite button", async t => {
+    await Selector("#indefNotif"); const indefNotification = Selector("#indefNotif");
+    await t 
+        .click(indefButton)
+        .expect(indefSpan.hasChildElements).eql(true);
+});
+
+test("Click definite button", async t => {
+    await t 
+        .click(defButton)
+        .expect(defSpan.hasChildElements).eql(true)
+        .wait(1000)
+        .expect(defSpan.hasChildElements).eql(false);
+});
+
+test("Click two buttons", async t => {
+    await t 
+        .click(defButton)
+        .click(indefButton)
+        .expect(defSpan.hasChildElements).eql(true);
+});
