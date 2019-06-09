af3155a58358e8380efed8649e0be69aa1b9b21c
Ricardo Molina <R3molina@.ucsd.edu>
2019-06-08 19:12:55 -0700

    deleted some files

--- src/raw-beer/Raw-Nav/raw-nav.js
+++ src/raw-beer/Raw-Nav/raw-nav.js
@@ -8,13 +8,8 @@ template.innerHTML = `
 `;
 
 /**
-<<<<<<< HEAD
  * @description Web-Component that functions as a navigation bar. 
  * Can be linked with beer-button and beer-nav components
-=======
- * @description A class which contains the beer-nav web component 
- * @example
->>>>>>> 1fd49e8ecb16e8d28e8357216abec2e3cf432e8d
  * 
  * <beer-navbar> </beer-navbar>
  */
@@ -58,7 +53,6 @@ class BeerNav extends HTMLElement {
         }
        
     }  
-<<<<<<< HEAD
 
     /**
      * @description When this function is called it gets a beer-brand element from the DOM 
@@ -67,16 +61,6 @@ class BeerNav extends HTMLElement {
      * TODO: the below needs to be tested
      * <beer-navbar navBarBrandID="beer"></beer-navbar>  
      * <beer-brand navBarBrandID="beer"></beer-brand> 
-=======
-    /**
-     * @description When this function is called it gets a beer-brand element from the DOM and then adds 
-     * it to the navBar by checking to see if the beer-brand and beer-navbar have the same navBarBrandid
-     * attribute if they do then the beer-brand is then brought connected to the beer-navBar
-     * @example
-     * 
-     * <beer-brand navbarBrandID="barbrand"> </beer-brand>
-     * <beer-navbar navbarBrandID="barbrand"> </beer-navbar>
->>>>>>> 1fd49e8ecb16e8d28e8357216abec2e3cf432e8d
      */
     getBrand(){
         var beerbrands = document.getElementsByTagName("beer-brand");
