64f749876363b190b4efe28599436fce83911a53
zhaoesther <ewzhao@ucsd.edu>
2019-06-12 16:28:30 -0700

    Notification backtracked

--- src/port-beer/spawnFunc.js
+++ src/port-beer/spawnFunc.js
@@ -1,13 +1,13 @@
 
 export function spawnNotifDefault(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit title="Button page" duration="1000">
+    var newNotification = `<beer-notification-lit title="Button page">
                             Notification Message here
-                          </beer-notification-lit>`;
+                           </beer-notification-lit>`;
     document.querySelector("body").insertAdjacentHTML("beforeend", newNotification); 
 }
 
 export function spawnNotifSuccess(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit type="success" title="Default" duration="4500">
+    var newNotification = `<beer-notification-lit type="success" title="Default" duration="7500">
                                 Congratulations class of 2019! You made it. Isn't that awesome :D
                                 This message is to be longer than the other ones to show that
                                 the notification will dynamically adjust its height depending on
@@ -18,28 +18,28 @@ export function spawnNotifSuccess(type, title, message, position, duration, hide
 }
 
 export function spawnNotifWarning(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit type="warning" title="Warning" duration="4500">
+    var newNotification = `<beer-notification-lit type="warning" title="Warning" duration="7500">
                             Notification Message here
                           </beer-notification-lit>`;
     document.querySelector("body").insertAdjacentHTML("beforeend", newNotification); 
 }
 
 export function spawnNotifInfo(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit type="info" title="Info" duration="4500">
+    var newNotification = `<beer-notification-lit type="info" title="Info" duration="7500">
                             Notification Message here
                           </beer-notification-lit>`;
     document.querySelector("body").insertAdjacentHTML("beforeend", newNotification); 
 }
 
 export function spawnNotifDanger(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit type="danger" title="Danger" duration="4500">
+    var newNotification = `<beer-notification-lit type="danger" title="Danger" duration="7500">
                             Notification Message here
                           </beer-notification-lit>`;
     document.querySelector("body").insertAdjacentHTML("beforeend", newNotification); 
 }
 
 export function spawnNotifMessage(type, title, message, position, duration, hideClose) {
-    var newNotification = `<beer-notification-lit type="message" title="Message" duration="4500">
+    var newNotification = `<beer-notification-lit type="message" title="Message" duration="7500">
                             Notification Message here
                           </beer-notification-lit>`;
     document.querySelector("body").insertAdjacentHTML("beforeend", newNotification); 
