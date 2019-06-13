892d75f5a2976a1cb8dda6b80a130bb4dd5f8a15
Ricardo Molina <R3molina@.ucsd.edu>
2019-06-13 13:10:11 -0700

    unittest 3748]

--- src/raw-beer/Raw-Nav/raw-nav.js
+++ src/raw-beer/Raw-Nav/raw-nav.js
@@ -1,5 +1,4 @@
 "use strict";
-import {AttributeSelector, setStyle} from "../Raw-Functions/NavBarFunctions.js";
 
 const template = document.createElement("template");
 template.innerHTML = `
@@ -27,7 +26,6 @@ class BeerNav extends HTMLElement {
         
         // This is the same as before Im making it so that all the variables here 
         // are available to Attribute Selector 
-        this.AttributeSelector = AttributeSelector.bind(this);
         this.AttributeSelector();
         
         var getBrand = this.getBrand.bind(this);
@@ -162,12 +160,8 @@ class BeerNav extends HTMLElement {
     getRegButtons(){
         // beerButtons is a variable that holds all the beer-buttons that were placed on the
         // page.
-        console.log("hellloooooooooooooooooo");
-        console.log("hellloooooooooooooooooo");
-
 
         var beerButtons = document.getElementsByTagName("button");
-        console.log("hello this lit button ", beerButtons);
         var navBarID = this.getAttribute("navBarID");
         for(var i =0 ; i < beerButtons.length; i++){
             var currBeerButton = beerButtons[i];
@@ -211,12 +205,7 @@ class BeerNav extends HTMLElement {
     getLitButtons(){
         // beerButtons is a variable that holds all the beer-buttons that were placed on the
         // page.
-        console.log("hellloooooooooooooooooo");
-        console.log("hellloooooooooooooooooo");
-
-
         var beerButtons = document.getElementsByTagName("beer-button-lit");
-        console.log("hello this lit button ", beerButtons);
         var navBarID = this.getAttribute("navBarID");
         for(var i =0 ; i < beerButtons.length; i++){
             var currBeerButton = beerButtons[i];
@@ -245,6 +234,77 @@ class BeerNav extends HTMLElement {
             }
         }
     }
+
+
+    /**
+    * @description Function that helps with selecting attributes
+    */
+    AttributeSelector(){
+    
+        if(this.hasAttribute("id")){
+            if(this.hasAttribute("beerId")){
+                this.$beerNav.setAttribute("id", this.getAttribute("beerId"));
+            }
+            this.$beerNav.setAttribute("id", this.getAttribute("id"));         
+        }
+        
+        if(this.hasAttribute("beerclass")){
+            if(this.getAttribute("beerclass").length > 0){
+                this.$beerNav.setAttribute("class", this.getAttribute("beerclass"));
+            }
+            else{
+                console.log("The class field is set to empty");
+            }
+        }
+        if(this.hasAttribute("beertype")){
+            if(this.getAttribute("beertype").length > 0){
+                this.$beerNav.setAttribute("type", this.getAttribute("beertype"));
+            }
+        }
+        // statement to control the flow of style so that newStyle has priority over
+        // libstyle
+        if(this.hasAttribute("newStyle") || this.hasAttribute("libStyle")){
+        // checks for a style and if it exists imports a .css style sheet
+            if(this.hasAttribute("newStyle")){
+                this.setStyle(this.getAttribute("newStyle"), this._shadowRoot);
+            }
+
+            else if(this.hasAttribute("libStyle")){
+                this.libStyle(this.getAttribute("libStyle"));
+            }
+        }
+    }
+
+    /**
+     * @description Function that allows for a custom style sheet to be applied
+     * @param {string} newStyle string that is the .css file to be imported
+     * @example
+     * 
+     * <beer-button newStyle="styles.css">Style Testing</beer-button>
+     * 
+     */
+    setStyle(newStyle, shadowRoot){
+        var Style = document.createElement("link");
+        shadowRoot.appendChild(Style);
+        Style.setAttribute("rel", "stylesheet");
+        Style.setAttribute("href", newStyle);
+        Style.setAttribute("type", "text/css");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 window.customElements.define("beer-navbar", BeerNav);
