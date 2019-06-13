ee0a200da6b3754c8e6f824f71059045a2baf511
Richard Pena <penarichard53@gmail.com>
2019-06-12 17:58:31 -0700

    removed majority of gulp automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,8 @@
 var gulp = require("gulp");
-var del = require("del");
-var uglify = require("gulp-uglify-es").default;
 var terser = require("gulp-terser");
-var runSequence = require("gulp4-run-sequence");
 var rename = require("gulp-rename");
 
-gulp.task("clean", function(done) {
-    del(["dist"]);
-    return done();
-});
-
-gulp.task("default", function(done) {
-    runSequence("clean", "terser");
-    return done();
-});
-
-gulp.task("terser", function() {
+gulp.task("default", function() {
     return gulp.src("./src/**/*.js")
     // Minify the file
         .pipe(terser())
@@ -25,7 +12,3 @@ gulp.task("terser", function() {
         .pipe(gulp.dest("dist"));
 
 });
-
-function done() {
-    console.log("Gulp build process complete");
-}
